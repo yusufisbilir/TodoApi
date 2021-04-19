0e08a421ca738b2ff5e0edd915b847edfdf77062
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-04-19 17:55:04 +0300

    add todos router

--- todoApi/app.js
+++ todoApi/app.js
@@ -7,6 +7,7 @@ const db = require('./helper/db.js')();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const todosRouter = require('./routes/todos.js');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/todos',todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ todoApi/routes/todos.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const todo = require('../models/todo.js');
+
+//List todos
+router.get('/', (req, res,next) => {
+    todo.find().then((todos)=>{
+        res.json(todos);
+    }).catch((err)=>{
+        res.json(err);
+    });
+});
+
+//Add new todo
+router.post('/', (req,res,next)=>{
+    new todo({
+        title:req.body.title,
+        date: req.body.date,
+        completed: req.body.completed,
+    }).save().then(()=>{
+        res.json('Added new todo item');
+    }).catch((err)=>{
+        res.json("Error - add todo");
+    });
+});
+
+//Update todo
+router.put('/:id',function(req,res,next){
+    const id = req.params.id;
+
+    todo.findByIdAndUpdate({"_id":id},req.body)
+    .then((newTodo)=>{
+        res.json("Updated todo");
+    })
+    .catch((err)=>{
+        res.json("Error - update todo")
+    });
+});
+
+//Delete todo
+router.delete(":_id",(req,res,next)=>{
+    const id = req.params.id;
+    todo.findByIdAndRemove(id)
+    .then(() => {
+        res.json("Deleted");
+    }).catch((err) => {
+        res.json("Error - deleting todo");
+    });
+});
+
+module.exports = router;
